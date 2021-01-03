34369ee988d96ee9751b49b439bb5b9b05a984ef
hiroyone <hirokiyonetani@gmail.com>
2021-01-03 12:06:15 +0900

    Seperate a map function from the main func

--- lib/sortedPostsData.tsx
+++ lib/sortedPostsData.tsx
@@ -2,6 +2,31 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
+function getPostsMetaData(
+  fileName: string,
+  postsDirectory: string
+): {
+  date: string;
+  title: string;
+  id: string;
+} {
+  // Remove ".md" from file name to get id
+  const id = fileName.replace(/\.md$/, "");
+
+  // Read markdown file as string
+  const fullPath = path.join(postsDirectory, fileName);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+  // console.log(matterResult);
+  // Combine the data with the id
+  return {
+    id,
+    ...(matterResult.data as { date: string; title: string }),
+  };
+}
+
 export default function getSortedPostsData(
   folder: string
 ): { date: string; title: string; id: string }[] {
@@ -9,23 +34,9 @@ export default function getSortedPostsData(
 
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, "");
-
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
-    // console.log(matterResult);
-    // Combine the data with the id
-    return {
-      id,
-      ...(matterResult.data as { date: string; title: string }),
-    };
-  });
+  const allPostsData = fileNames.map((fileName) =>
+    getPostsMetaData(fileName, postsDirectory)
+  );
 
   // Sort posts by date
   return allPostsData.sort((a, b) => {
