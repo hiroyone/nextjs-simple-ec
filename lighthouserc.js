572946cbd03a17c44863be9df307bd07c03e2d92
hiroyone <hirokiyonetani@gmail.com>
2021-01-16 21:34:13 +0900

    Add lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 1,
+      staticDistDir: "./out",
+      url: ["/"],
+    },
+    assert: {
+      preset: "lighthouse:no-pwa",
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
